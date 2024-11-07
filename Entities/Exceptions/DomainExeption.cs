01f01eed89df0d6562dc66044b1712642e53bb9c
DaylonFellipe <daylonfsouza@gmail.com>
2024-11-07 15:32:27 +0000

    teste

---
+++ Entities/Exceptions/DomainExeption.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ProjetoOportunidade.Entities.Exceptions
+{
+    //DomainExeption é uma subclasse de ApplicationException
+    internal class DomainExeption : ApplicationException
+    {
+        //Tem um construtor recebendo um string message, e repassa essa mensagem para o construtor da classe ApplicationException
+        public DomainExeption(string message) : base(message) { }
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Globalization;
+using ProjetoOportunidade.Entities;
+using ProjetoOportunidade.Entities.Exceptions;
+
+
+namespace ProjetoOportunidade
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("Room Number: ");
+                int number = int.Parse(Console.ReadLine());
+                Console.Write("Check-in date: ");
+                DateTime checkIn = DateTime.Parse(Console.ReadLine());
+                Console.Write("Check-iout date: ");
+                DateTime checkOut = DateTime.Parse(Console.ReadLine());
+
+                Reservation reservation = new Reservation(number, checkIn, checkOut);
+                Console.WriteLine("Reservation " + reservation);
+
+                Console.WriteLine();
+                Console.WriteLine("Enter data to update the reservation: ");
+
+                Console.Write("Check-in date: ");
+                checkIn = DateTime.Parse(Console.ReadLine());
+                Console.Write("Check-iout date: ");
+                checkOut = DateTime.Parse(Console.ReadLine());
+
+                reservation.UpdateDates(checkIn, checkOut);
+                Console.WriteLine("Reservation " + reservation);
+            }
+            catch (DomainExeption e)
+            {
+                Console.WriteLine("Error in reservation: " + e.Message);
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine("Format error: " + e.Message);
+            }
+            Console.ReadKey();
+        }
+    }
+}
+
